9cb7873a5743146355bb1a413fc0af2fe9b21fba
Mulyar Vlad <mulyarvlad@MulyarVlad.local>
2023-02-05 19:09:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let userFirstNumber = +prompt("Enter the first number");
+
+let userSecondNumber = +prompt("Enter the second number");
+
+let userThirdNumber = +prompt("Enter the third number");
+
+alert((userFirstNumber + userSecondNumber + userThirdNumber) / 3);
